ebeae1748407e20b5693b11dd6abd533635bf34f
shakil-singularity <shakil.singularity@gmail.com>
2024-04-08 10:37:06 +0600

    first commit

---
+++ src/pages/success.js
@@ -0,0 +1,9 @@
+export default function Success() {
+  return (
+    <main className="flex min-h-screen">
+      <div className="">
+        <p className="">Form Submitted</p>
+      </div>
+    </main>
+  );
+}
